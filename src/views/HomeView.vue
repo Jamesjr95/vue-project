b518d762d6bce67aed0934ab63aad0d383d97a5c
James Johnson <jamesjhnsn1995@gmail.com>
2023-01-19 12:05:19 -0800

    added key

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -78,7 +78,7 @@ const getSearchResults = () => {
     if (searchQuery.value !== ''){
       try {
         // const result = await axios.get(`${apiUrl}?q=${searchQuery.value}.json`)
-        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}?access_token=pk.eyJ1IjoiamF5MTk5NSIsImEiOiJjbGNrcnRpM2cwMjQ3M29ubTc5cjMyajFtIn0.hwGbM_Dn8EHwnnAFtBqvyQ&types=place`)
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=pk.eyJ1IjoiamF5MTk5NSIsImEiOiJjbGNrcnRpM2cwMjQ3M29ubTc5cjMyajFtIn0.hwGbM_Dn8EHwnnAFtBqvyQ&types=place`)
         searchResults.value = result.data.features;
       } catch {
         searchError.value = true
